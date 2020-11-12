970c44ed8bf82f0154681310ea335938ddadfb63
chena <chena@foxhis.com>
2020-11-13 00:23:59 +0800

    删除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome三生三世 to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
