00dffe9ae0969c44b3118fc5f0f6a1b9569dee71
MarioGogogo <148373644@qq.com>
2020-11-28 17:44:49 +0800

    取消vue-toastification组件

--- src/main.js
+++ src/main.js
@@ -1,32 +1,41 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-13 21:44:25
+ * @LastEditTime: 2020-11-28 17:44:44
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /example-demo/src/main.js
+ */
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Toast from 'vue-toastification';
+// import Toast from 'vue-toastification';
 // Toast 样式
-import 'vue-toastification/dist/index.css';
+// import 'vue-toastification/dist/index.css';
 import VueDND from 'awe-dnd'
 // 导入拖拽组件
 Vue.use(VueDND)
 
 
-Vue.use(Toast, {
-  transition: 'Vue-Toastification__bounce',
-  maxToasts: 3,
-  newestOnTop: true,
-  position: 'top-right',
-  timeout: 2000,
-  closeOnClick: true,
-  pauseOnFocusLoss: true,
-  pauseOnHover: false,
-  draggable: true,
-  draggablePercent: 0.7,
-  showCloseButtonOnHover: false,
-  hideProgressBar: true,
-  closeButton: 'button',
-  icon: true,
-  rtl: false,
-});
+// 暂时不用这个tosash组件
+// Vue.use(Toast, {
+//   transition: 'Vue-Toastification__bounce',
+//   maxToasts: 3,
+//   newestOnTop: true,
+//   position: 'top-right',
+//   timeout: 2000,
+//   closeOnClick: true,
+//   pauseOnFocusLoss: true,
+//   pauseOnHover: false,
+//   draggable: true,
+//   draggablePercent: 0.7,
+//   showCloseButtonOnHover: false,
+//   hideProgressBar: true,
+//   closeButton: 'button',
+//   icon: true,
+//   rtl: false,
+// });
 
 
 import Vant from 'vant';
